ee5e7c8d8ad2cd5ced190ebdcc37714e484f0b38
abamishi <abamishi@xin720945.speedport.ip>
2023-03-11 18:31:19 +0100

    if applied, would add a source file

---
+++ src/CitySearch.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+
+class CitySearch extends Component {
+  state = {
+    query: "",
+    suggestions: [],
+    showSuggestions: undefined,
+  };
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({ showSuggestions: true });
+    const suggestions = this.props.locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        suggestions: [],
+        showSuggestions: false,
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions: suggestions,
+      });
+    }
+  };
+
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion,
+      suggestions: [],
+      showSuggestions: false,
+    });
+
+    this.props.updateEvents(suggestion);
+  };
+
+  handleInputFocus = () => {
+    this.setState({ showSuggestions: true });
+  };
+
+  render() {
+    return (
+      <div className="CitySearch">
+        <h3>Find a City</h3>
+        <input
+          type="text"
+          className="city"
+          placeholder="Search"
+          value={this.state.query}
+          onChange={this.handleInputChanged}
+          onFocus={this.handleInputFocus}
+        />
+        <ul
+          className="suggestions"
+          style={this.state.showSuggestions ? {} : { display: "none" }}
+        >
+          {this.state.suggestions.map((suggestion) => (
+            <li
+              key={suggestion}
+              onClick={() => this.handleItemClicked(suggestion)}
+            >
+              {suggestion}
+            </li>
+          ))}
+          <li key="all" onClick={() => this.handleItemClicked("all")}>
+            <h4>See all cities</h4>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default CitySearch;
---
+++ src/NumberOfEvents.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class NumberOfEvents extends Component {
+  state = { noe: 32 };
+
+  handleInputChanged = (event) => {
+    const inputValue = event.target.value;
+    this.props.updateEvents(null, inputValue);
+    this.setState({
+      noe: inputValue,
+    });
+  };
+
+  render() {
+    const { noe } = this.state;
+    return (
+      <div className="NumberOfEvents">
+        <h3># of Events:</h3>
+        <input
+          className="noe-input"
+          type="number"
+          value={noe}
+          onChange={(event) => {
+            this.handleInputChanged(event);
+          }}
+        ></input>
+      </div>
+    );
+  }
+}
+
+export default NumberOfEvents;

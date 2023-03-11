0f1ed4bb6efd4dd9cfca9e6467890d3a98d49366
abamishi <abamishi@xin720945.speedport.ip>
2023-03-11 19:13:53 +0100

    Updates

---
+++ precache-manifest.e58bd0c8f44c76b41c29c5d1eb00c978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "88530e92f324be85e871",
+    "url": "/meet-luisa/static/css/main.8a0a5549.chunk.css"
+  },
+  {
+    "revision": "88530e92f324be85e871",
+    "url": "/meet-luisa/static/js/main.91c8a665.chunk.js"
+  },
+  {
+    "revision": "03fd98932a0492805088",
+    "url": "/meet-luisa/static/js/runtime~main.9a8e4c94.js"
+  },
+  {
+    "revision": "4830713c89bc65aaac5c",
+    "url": "/meet-luisa/static/js/2.42f51591.chunk.js"
+  },
+  {
+    "revision": "03c2247ebef71b42a125",
+    "url": "/meet-luisa/static/js/3.133d7e0c.chunk.js"
+  },
+  {
+    "revision": "69b5d30de959e98d51ffb855ec5820bf",
+    "url": "/meet-luisa/index.html"
+  }
+];
